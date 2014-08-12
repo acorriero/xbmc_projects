68cfa3d628fea85557420caf68ea851e1232eaa7
acorriero <anthony.corriero@gmail.com>
2014-08-11 20:11:40 -0400

    Making changes

---
+++ plugin.video.canalalgerie/util.py
@@ -0,0 +1,33 @@
+import xbmcgui, xbmc
+
+def playMedia(title, thumbnail, link, mediaType='Video'):
+    '''Plays a video
+    Arguments:
+    title: the title to be displayed
+    thumbnail: the thumbnail to be used as an icon
+    link: the link to the media to be played
+    mediaType: the type of media to play: Video, Pictures, Music, or Programs
+    '''
+    li = xbmcgui.ListItem(label=title, iconImage=thumbnail, thumbnailImage=thumbnail, path=link)
+    li.setInfo(type=mediaType, infoLabels={ "Title": title })
+    xbmc.Player().play(item=link, listitem=li)
+    
+def parseParameters(inputString=sys.argv[2]):
+    """Parses a parameter string starting at the first ? found in inputString
+    
+    Argument:
+    inputString: the string to be parsed, sys.argv[2] by default
+    
+    Returns a directory with parameters names as keys and parameter values as values
+    """
+    parameters = {}
+    p1 = inputString.find('?')
+    if p1 >= 0:
+        splitParameters = iputString[p1 + 1].split('&')
+        for nameValuePair in splitParameters:
+            if len(nameValuePair) > 0:
+                pair = nameValuePair.split('=')
+                key = pair[0]
+                value = urllib.unquote(urllib.unquote_plus(pair[1])).decode('utf-8')
+                paramaters[key] = value
+    return parameters
